8e7d027c4f069264814e58a9849fe87e9c921e3a
Daniel Grant <1670902+djgrant@users.noreply.github.com>
2020-10-16 19:34:17 +0200

    Add storybook examples

---
+++ stories/code-input.stories.tsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import { Meta, Story } from '@storybook/react';
+import { CodeInput, CodeInputProps } from '../src/code-input';
+
+const meta: Meta = {
+  title: 'CodeInput',
+  component: CodeInput,
+  argTypes: {
+    operators: { control: { type: 'array' } },
+    variables: { control: { type: 'array' } },
+  },
+  args: {
+    operators: ['+', '-', '/', '*'],
+    variables: [
+      'accruedInterest',
+      'adjustedDiscountPrice',
+      'commision',
+      'costs',
+      'cpa',
+      'cvr',
+      'price',
+      'profit',
+      'salePrice',
+      'vat',
+    ],
+    placeholder: 'salePrice - vat',
+  },
+  parameters: {
+    controls: { expanded: true },
+  },
+};
+
+export default meta;
+
+const Template: Story<CodeInputProps> = args => <CodeInput {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {};
+
+export const CustomStyles = Template.bind({});
+CustomStyles.args = {
+  style: {
+    width: '350px',
+    padding: '10px',
+    fontSize: '14px',
+    fontFamily: 'monospace',
+  },
+};
+
+const ExampleCustomInput = React.forwardRef<HTMLInputElement>((props, ref) => (
+  <input className="input" ref={ref} {...props} />
+));
+
+export const CustomComponent = Template.bind({});
+CustomComponent.args = {
+  customInput: ExampleCustomInput,
+};
